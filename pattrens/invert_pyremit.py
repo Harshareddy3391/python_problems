4478d06d01fa23e59c44da66f40d8c36a656079e
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-29 14:51:45 +0530

    n

--- pattrens/invert_pyremit.py
+++ pattrens/invert_pyremit.py
@@ -1,10 +1,11 @@
 n=6
 
 for i in range(n):
+    for a in range(0,i+1):
+         print("1",end=" ")
     for j in range(0,n-i):
         print("*",end=" ")
 
-    for a in range(0,i+1):
-        print("1",end=" ")
+    
 
     print()
