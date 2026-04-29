677fbba4e9f1a1cc71483cecce6a208ec6d47c94
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-29 15:52:32 +0530

    practice loops

--- pattrens/invert_pyremit.py
+++ pattrens/invert_pyremit.py
@@ -12,5 +12,12 @@ for i in range(n):
 
 
 n = 5
-for i in range(n, 0, -1):
+"""for i in range(n, 0, -1):
     print("1"*(n-i) + "*2"*i)
+"""
+for j in range(n,1,-1):
+    print(" "*(n-j) + "* "*j)
+
+
+for i in range(1,n):
+    print(" "*(n-i-1) + " *"*i)
