e1133f16e9ce5683a841b00730df49d5afcf91a5
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-29 15:41:19 +0530

    corner

--- pattrens/invert_pyremit.py
+++ pattrens/invert_pyremit.py
@@ -1,4 +1,4 @@
-n=6
+"""n=6
 
 for i in range(n):
     for a in range(0,i+1):
@@ -8,4 +8,9 @@ for i in range(n):
 
     
 
-    print()
+    print()"""
+
+
+n = 5
+for i in range(n, 0, -1):
+    print("1"*(n-i) + "*2"*i)
